cc0e76756e67bf454baaac03b1ee988d47cf3516
conradgann <conrad@bwmacro.com>
2023-11-30 17:46:04 -0800

    checkbox

--- day8.py
+++ day8.py
@@ -5,6 +5,23 @@ import pandas as pd
 import numpy as np
 
 
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
 st.header('st.multiselect')
 
 options = st.multiselect(

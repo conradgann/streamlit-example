a3b7045249a7c7517783bd964748e33d27ab416c
conradgann <conrad@bwmacro.com>
2023-11-30 17:37:50 -0800

    line chart

--- day8.py
+++ day8.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from datetime import time, datetime
 
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
 st.header('st.slider')
 
 # Example 1

7e1e9d6bf926e6cf7c9d2addc5ec57608b16a670
conradgann <conrad@bwmacro.com>
2023-12-02 12:16:50 -0800

    day 14

--- day8.py
+++ day8.py
@@ -1,10 +1,18 @@
 import streamlit as st
 from datetime import time, datetime
-
+````
 import pandas as pd
 import numpy as np
 
+import ydata_profiling
+from streamlit_pandas_profiling import st_profile_report
 
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
 
 st.header('st.checkbox')
 
